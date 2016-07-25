4554c2932c5c5df713d17c9bfc877099eac98c79
timpal0l <timisbister@gmail.com>
2016-07-25 17:48:38 +0200

    :PPPPPPPPPPPPP

---
+++ crawler/main.py
@@ -0,0 +1,16 @@
+import blogsscraper as bs
+import textscraper as ts
+import time
+
+
+def main():
+    # bs.get_profiles(100)
+    bs.get_blog()
+    ts.get_text()
+    print("1")
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print('\n--- %s seconds ---' % (time.time() - start_time))
